d0a90780acc9b97cd689378b38bc6766720b6a26
j828h <j828h@naver.com>
2021-10-26 10:14:40 +0900

    #S05P31A404-205 Feat: add [BE] findAllGoods

---
+++ V-Ket/backend/src/main/java/com/vket/api/service/GoodsService.java
@@ -0,0 +1,20 @@
+package com.vket.api.service;
+
+import com.vket.api.response.GoodsRes;
+
+import java.util.List;
+
+public interface GoodsService {
+//    List<Goods> findAll();
+    List<GoodsRes> findAllGoods();
+
+    // 상점 별 전체 상품 정보
+
+    // 선택된 상품의 상세 정보
+
+    // 상품 정보 사정하기
+
+    // 상품 등록하기
+    
+    // 상품 삭제하기
+}
