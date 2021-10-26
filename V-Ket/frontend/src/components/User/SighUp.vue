a841aedb6fe304a4b21e66e37cc8c4126dd6f6a1
심재우 <jw_shim@naver.com>
2021-10-26 13:58:48 +0900

    #S05P31A404-142 Feat: add [FE] SignUp component

---
+++ V-Ket/frontend/src/components/User/SighUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    아이디: <input v-model="userId"/><br>
+    비밀번호: <input v-model="userPassword"/><br>
+    닉네임: <input v-model="userNickname"/><br>
+    전화번호: <input v-model="userPhone"/><br>
+    <button @click="sighUp()"> 회원가입 </button>
+  </div>
+</template>
+<script>
+import http from '@/http.js';
+
+export default {
+  name: 'SignUp',
+
+  data() {
+    return {
+      userSeq: '',
+      userId: '',
+      userNickname: '',
+      userPhone: '',
+      userToken: '',
+      userPassword: ''
+    };
+  },
+  methods: {
+    sighUp() {
+      const body = {
+        userId : this.userId,
+        userPassword : this.userPassword,
+        userNickname : this.userNickname,
+        userPhone : this.userPhone
+      }
+      http.post('/user/signUp', body)
+      .then((res)=>{
+        alert(res+"가입성공!")
+      })
+      location.reload();
+    },
+  },
+};
+</script>
